9f42d6a7a397a2f129f8742b9129fa6bde451f44
Angelo García <161162882+ANHELL0S@users.noreply.github.com>
2024-11-17 23:05:16 -0500

    Add files via upload

---
+++ theme.bash
@@ -0,0 +1,89 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Manhattan Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#282828'
+foreground='#f1fcf9'
+color0='#282828'
+color1='#db86ba'
+color2='#74dd91'
+color3='#e49186'
+color4='#75dbe1'
+color5='#e6cb6a'
+color6='#9ee9ea'
+color7='#f1fcf9'
+color8='#465463'
+color9='#d04e9d'
+color10='#4bc66d'
+color11='#db695b'
+color12='#3dbac2'
+color13='#825ece'
+color14='#62cdcd'
+color15='#e0e5e5'
+
+element_bg='#282828'
+element_fg='#F0FBF8'
+element_hl='#D5C4A1'
+
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+#wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+#wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=9.4;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='12'
+
+# Geany
+geany_colors='gruvbox.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='JetBrainsMono Nerd Font'
+gtk_theme='Gruvbox'
+icon_theme='Luv-Folders-Dark'
+cursor_theme='Qogirr-Dark'
+
+# Dunst
+dunst_width='250'
+dunst_height='80'
+dunst_offset='8x8'
+dunst_origin='buttom-right'
+dunst_font='JetBrainsMono Nerd Font 9'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='14'
+picom_shadow_r='14'
+picom_shadow_o='0.35'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='dual_kawase'
+picom_blur_strength='4'
+background = true;
+background-frame = false;
+background-fixed = true;
+kern="3x3box";
+
+# Bspwm
+bspwm_fbc="$element_hl"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='8'
+bspwm_sratio='0.50'
